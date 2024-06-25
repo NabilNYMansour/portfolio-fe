e8aac3f5c291373da67730ee42d0e6319db60c3b
Nabil Mansour <nabilmansour1999@gmail.com>
2024-06-25 19:31:08 -0400

    tweaked buy me coffee

--- app/ui/components/BuyMeCoffee.tsx
+++ app/ui/components/BuyMeCoffee.tsx
@@ -4,10 +4,10 @@ import classes from "./BuyMeCoffee.module.css";
 const BuyMeCoffee = () => {
   return (
     <Container size="md" ta="center">
-      <h3>If you enjoyed this article and would like to support me, consider</h3>
+      <h3>If you enjoyed this article and would like to support me, consider to</h3>
       <a className={classes.coffee} href="https://buymeacoffee.com/nabilmansour" target="_blank">
         <h1 className={classes.coffeeInner}>
-          ☕️ buying me coffee ツ
+          ☕️ Buy me coffee ツ
         </h1>
       </a>
     </Container>
